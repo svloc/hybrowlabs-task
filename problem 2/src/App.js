48e88c7df827d2970e1b929b7683fd8a2b52f0bc
svloc <svloc39@gmail.com>
2022-04-23 00:40:46 +0530

    up

--- problem 2/src/App.js
+++ problem 2/src/App.js
@@ -2,15 +2,35 @@ import React,{useState,useEffect} from "react";
 import "./style.css";
 
 export default function App() {
+
   const data=['sachin','lokare','ABC',"XYZ"];
-  const delete_name=(index)=>{
-   data.splice(index,1);
-  console.log(index)
+  let arr=[];
+  const APL_URL='https://swapi.dev/api/people/';
+  const [nameData,setNameData]=useState();
+  const [loading,setLoading]=useState(false)
+
+  const delete_name=()=>{
+ 
   }
+  const get_name= ()=>{
+   
+      const min = 1;
+      const max = 83;
+      const random = Math.floor(min + (Math.random() * (max - min)));
+      fetch(APL_URL+random).then(res=> res.json()).then(result=>{
+        console.log(result.name)
+        setNameData(result.name);
+        arr.push(result);
+        setLoading(true)
+      })
+      console.log('array',arr)
+  }
+  console.log(nameData);
+ 
   return (
     <div className="container">
       <div className="pb-1">
-       <button className="btn btn-black">Add +</button>
+       <button className="btn btn-black" onClick={get_name}>Add +</button>
       </div>
      <div>
        <table>
@@ -22,14 +42,22 @@ export default function App() {
          </tr>
          </thead>
          <tbody>
-           {data.map((ele,index)=>{
-            return(<tr key={index}>
-              <td>{ele}</td>
-              <td><button className="btn btn-red" type="button" onClick={()=>delete_name(index)}>Delete</button></td>
-              </tr>)
-            
-           })}
-           
+           {loading &&
+           <>
+           {arr.map(ele=>{
+           return(<tr>
+              <td>{ele.name}</td>
+              <td><button className="btn btn-red" type="button" onClick={()=>delete_name()}>Delete</button></td>
+              </tr>);
+              
+            })}
+              </>
+      
+           }
+
+           {!loading && <>
+           <tr>No data there</tr>
+            </>}
            
            </tbody>
        </table>
