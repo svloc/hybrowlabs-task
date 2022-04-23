1c953c8fa254988edc39fb7888b1112631b1396d
svloc <svloc39@gmail.com>
2022-04-24 00:19:13 +0530

    last Change

--- problem 2/src/App.js
+++ problem 2/src/App.js
@@ -7,30 +7,30 @@ export default function App() {
   let arr=[];
   const APL_URL='https://swapi.dev/api/people/';
   const [nameData,setNameData]=useState();
-  const [loading,setLoading]=useState(false)
+  const [loading,setLoading]=useState(true)
 
   const delete_name=()=>{
- 
+    if(nameData){
+   setNameData('');
+   setLoading(false);
+    }
   }
   const get_name= ()=>{
-   
       const min = 1;
       const max = 83;
       const random = Math.floor(min + (Math.random() * (max - min)));
       fetch(APL_URL+random).then(res=> res.json()).then(result=>{
-        console.log(result.name)
         setNameData(result.name);
-        arr.push(result);
-        setLoading(true)
+        setLoading(true);
       })
-      console.log('array',arr)
+     
   }
-  console.log(nameData);
+
  
   return (
     <div className="container">
       <div className="pb-1">
-       <button className="btn btn-black" onClick={get_name}>Add +</button>
+       <button className="btn btn-black" type="button" onClick={get_name}>Add +</button>
       </div>
      <div>
        <table>
@@ -43,20 +43,15 @@ export default function App() {
          </thead>
          <tbody>
            {loading &&
-           <>
-           {arr.map(ele=>{
-           return(<tr>
-              <td>{ele.name}</td>
+           <tr>
+              <td>{nameData}</td>
               <td><button className="btn btn-red" type="button" onClick={()=>delete_name()}>Delete</button></td>
-              </tr>);
-              
-            })}
-              </>
+            </tr>
       
            }
 
            {!loading && <>
-           <tr>No data there</tr>
+           <p >No data there</p>
             </>}
            
            </tbody>
